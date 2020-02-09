f842f68670e0e582a44cc421c94199366b0318b1
hikerpig <hikerpigwinnie@gmail.com>
2020-02-09 13:17:10 +0800

    fix: :arrow_up: upgrade @unisnips/* to 0.8.0 fix nested script code offset bug

--- src/settings/example.ts
+++ src/settings/example.ts
@@ -24,6 +24,10 @@ snippet nested "nested tabstop"
 \${0:outer and \${1:inner}}
 endsnippet
 
+snippet script_inside_tabstop "js script inside tabstop"
+\${1:\`!js new Date()\`}
+endsnippet
+
 snippet tabstop_transformation "import vue component"
 import \${1/(.*)\\/([\\w]*)\\.vue/$2/g} from '\${1}'
 endsnippet
